b6167939b61cdb4758b9a37f73a2f7a23729b433
tomkeb <tomaskeb@gmail.com>
2017-12-28 22:46:25 +0100

    soubory

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+  <head>
+  <title>Verzování</title>
+  <meta charset="UTF-8">
+  <link href="styl.css" rel="stylesheet">
+  <link rel="shortcut icon" href="favicon.svg" type="image/svg+xml" />
+  </head>
+  <body>
+
+
+<form method="post" enctype="multipart/form-data" action="upload.php">
+Račte vybrat soubor:<br />
+<input type="file" name="souborknahrani" id="souborknahrani" /><br />
+Račte vložit svůj e-mail:<br />
+<input type="text" name="mail" id="mail" value="" /><br />
+
+<input type="submit" value="Nahrát" name="Nahrát">
+</form>
+
+</body>
+</html>
