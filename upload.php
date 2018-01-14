cf510a7039e1f91e39dc974314aed8728ca7763a
Petr Novotný <krkabol@gmail.com>
2018-01-14 16:56:58 +0100

    use vždy hned nahoru společně

--- upload.php
+++ upload.php
@@ -1,21 +1,21 @@
 <?php
+use Model\Proposal as Proposal;
+use Model\Storage as Storage;
+use Model\File as File;
 
 spl_autoload_register("loadClass");
 function loadClass($class)
 {
-  $file = str_replace('\\',DIRECTORY_SEPARATOR,$class);
-  require_once $file . '.php';
+    $file = str_replace('\\',DIRECTORY_SEPARATOR,$class);
+    require_once $file . '.php';
 }
 
 $fileToUpload = "Halí belí, koně v zelí.";
 $user = "student PedF UK";
 $toDir = "nahrane";
 
-use Model\Proposal as Proposal;
 $prop = new Proposal ($fileToUpload, $user);
 
-use Model\Storage as Storage;
 $stor = new Storage ($prop->hash, $toDir);
 
-use Model\File as File;
 $file = new File ($stor->toDir, $fileToUpload);
